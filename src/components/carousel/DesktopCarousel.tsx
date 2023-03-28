37b9b966ed8b47542e0e5c1a7682535168e25c6a
Temitope Asama <temitopeasama@gmail.com>
2023-03-28 23:50:01 +0100

    ✨removed redundant code✨

--- src/components/carousel/DesktopCarousel.tsx
+++ src/components/carousel/DesktopCarousel.tsx
@@ -1,10 +1,6 @@
 import React from 'react'
 import CarouselItem from './CarouselItem'
 import { carouselItems } from './MobileCarousel'
-import imgOne from '../../../public/images/image-product-1.jpg'
-import imgTwo from '../../../public/images/image-product-2.jpg'
-import imgThree from '../../../public/images/image-product-3.jpg'
-import imgFour from '../../../public/images/image-product-4.jpg'
 
 const DesktopCarousel = () => {
     
