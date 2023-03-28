e9c11bb1f74dd1b8b872b9eab75ed56a96b60242
Temitope Asama <temitopeasama@gmail.com>
2023-03-29 00:22:39 +0100

    carousel navigation✨

--- src/components/carousel/DesktopCarousel.tsx
+++ src/components/carousel/DesktopCarousel.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from 'next/image'
 import CarouselItem from './CarouselItem'
 import { carouselItems } from './MobileCarousel'
 
@@ -6,11 +7,28 @@ const DesktopCarousel = () => {
     
   return (
     <div>
-        {carouselItems.map((item) => {
-            return <CarouselItem key={item.key} image={item.image}/>
-        })}
+        <div>
+            {carouselItems.map((item) => {
+                return <CarouselItem key={item.key} image={item.image}/>
+            })}
+        </div>
+        <div className='button__container'>
+            {carouselItems.map((item) => {
+                return (
+                <button className="carousel__navigation" key={item.key}>
+                    <Image
+                        src={item.image}
+                        alt="Picture of the author"
+                        width={50}
+                        height={50}
+                        style={{objectFit:"cover"}}
+                    />
+                </button>
+            )})}
+        </div>
     </div>
   )
 }
 
+// <button className="carousel__navigation"></button>
 export default DesktopCarousel
